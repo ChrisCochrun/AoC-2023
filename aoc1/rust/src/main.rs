2a8cc8a145b9d6c5528eb0f8bd637f1d890f02b0
Chris Cochrun <chris@cochrun.xyz>
2023-12-01 16:53:17 -0600

    the second version.... unfinished

--- aoc1/rust/src/main.rs
+++ aoc1/rust/src/main.rs
@@ -1,33 +1,76 @@
-use std::{
-    fs::{read_to_string, File},
-    path::Path,
-};
+use std::fs::read_to_string;
 
 fn main() {
     if let Ok(string) = read_to_string("input.txt") {
         let vec: Vec<&str> = string.split('\n').collect();
         let mut sum = 0;
+        let num_strs = vec![
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ];
         for i in vec {
             if i.is_empty() {
-                println!("{sum}");
+                // println!("{sum}");
                 return;
             }
-            let first_int = i.trim_start_matches(char::is_alphabetic).get(..1).unwrap();
+            let first_int_pos = i.find(char::is_numeric).unwrap();
+            let last_int_pos = i.rfind(char::is_numeric).unwrap();
+            let mut first_int = String::from("0");
+            let mut last_int_string = String::from("0");
             let last_index = i.trim_end_matches(char::is_alphabetic).len() - 1;
             let (_index, last_int) = i
                 .char_indices()
                 .filter(|(i, _x)| i == &last_index)
                 .next()
                 .unwrap_or_else(|| (0, char::from_u32(0).unwrap()));
-            // let last_int = last_int.to_digit(10).unwrap();
+            println!("{i}");
+            for (index, strs) in num_strs.clone().iter().enumerate() {
+                if let Some(first) = i.find(strs) {
+                    if first < first_int_pos {
+                        println!("THIS is FIRST: {}", first);
+                        println!("THIS is not: {}", first_int_pos);
+                        first_int = (index + 1).to_string();
+                        break;
+                    } else {
+                        first_int = i
+                            .trim_start_matches(char::is_alphabetic)
+                            .get(..1)
+                            .unwrap()
+                            .to_string();
+                    }
+                } else {
+                    first_int = i
+                        .trim_start_matches(char::is_alphabetic)
+                        .get(..1)
+                        .unwrap()
+                        .to_string();
+                }
+                if let Some(last) = i.rfind(strs) {
+                    if last > last_int_pos {
+                        println!("REAL LAST: {}", last);
+                        println!("REAL LAST: {}", strs);
+                        println!("NOT LAST: {}", last_int_pos);
+                        last_int_string = (index + 1).to_string();
+                        println!("REAL LAST: {}", last_int_string);
+                        break;
+                    }
+                }
+            }
+
             let mut cv = first_int.to_string();
-            cv.push(last_int);
+            if last_int_string.parse::<i32>().unwrap() > last_int.to_string().parse().unwrap() {
+                let chars: Vec<char> = last_int_string.chars().collect();
+                cv.push(*chars.first().unwrap());
+            } else {
+                cv.push(last_int);
+            }
+
+            // let last_int = last_int.to_digit(10).unwrap();
             let calibration_value: i32 = cv.parse().unwrap();
 
             println!("first: {:?}", first_int);
             println!("last: {:?}", last_int);
-            println!("{i}");
-            println!("{:?}", calibration_value);
+            // println!("{i}");
+            // println!("{:?}", calibration_value);
             sum = sum + calibration_value;
         }
     }
