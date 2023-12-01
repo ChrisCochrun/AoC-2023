1932cb5adc7c64b8b46b10a7d2db4852b58f5d8c
Chris Cochrun <chris@cochrun.xyz>
2023-12-01 10:14:48 -0600

    adding flake

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "Advent of Code";
+  # This flake will setup a lisp and rust environment to try AoC in both languages
+  inputs = {
+    # cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    # nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in rec
+        {
+          devShell = {
+            with pkgs;
+            mkShell rec {
+              name = "aoc";
+              
+              nativeBuildInputs = [
+                gcc
+              ];
+
+              buildInputs = [
+                stdenv
+                gnumake
+                pkg-config
+                
+                # Rust tools
+                clippy
+                rustc
+                cargo
+                rustfmt
+                rust-analyzer
+              ];
+
+              RUST_BACKTRACE = "full";
+              CARGO_PROFILE_RELEASE_BUILD_OVERRIDE_DEBUG = true;
+            }
+          };
+        }
+      );
+}
