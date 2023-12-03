7c9b1f184ced40060fc1927310a08a28405efdcf
Chris Cochrun <chris@cochrun.xyz>
2023-12-03 07:18:21 -0600

    adding rust day2 start

---
+++ day2/target/debug/day2.d
@@ -0,0 +1,1 @@
+/home/chris/dev/AoC-2023/day2/target/debug/day2: /home/chris/dev/AoC-2023/day2/src/main.rs
---
+++ day2/target/debug/deps/day2-1057bea2ad12c732.d
@@ -0,0 +1,5 @@
+/home/chris/dev/AoC-2023/day2/target/debug/deps/day2-1057bea2ad12c732.rmeta: src/main.rs
+
+/home/chris/dev/AoC-2023/day2/target/debug/deps/day2-1057bea2ad12c732.d: src/main.rs
+
+src/main.rs:
